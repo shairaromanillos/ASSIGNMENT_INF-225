6d18a2d76738d848bcd7d0c65e595caad02d9044
SHAIRA <122065395+shairaromanillos@users.noreply.github.com>
2023-02-10 10:07:15 +0800

    Create Person.java

---
+++ Exercise5/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+
+    // Private variable
+    // Private variables are only accessible within the class where they are
+    // declared. They are not accessible outside of this class.
+    private String name;
+    private int ccnumber;
+
+    // get() method
+    public String getName() {
+
+        return this.name;
+    }
+
+    // set() method
+    public void setName(String newName) {
+
+        this.name = newName;
+    }
+
+    // set() method
+    public void setCCnumber(int cardnumber) {
+
+        this.ccnumber = cardnumber;
+    }
+
+       // get() method
+       public int getCCnumber() {
+
+        return this.ccnumber;
+    }    
+}
